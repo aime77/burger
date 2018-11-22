0ea4ddd36ae0dc25ab2a1e69d237e55ffab3220b
Jeme <aime.urquieta@outlook.com>
2018-11-22 17:25:55 -0600

    heroku error fix

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,27 @@
 //require('./app.dotenv').config();
+
 const mysql = require('mysql');
 let connection;
 
-process.env.NODE_ENV === 'production' ? connection = mysql.createConnection(JAWSDB_URL) :
-    connection = mysql.createConnection({
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
-        password: process.env.DB_PASSWORD || 'root',
-        user: process.env.DB_USER || 'root',
-        database: process.env.DB_DATABASE || 'burger_db'
-    });
 
-connection.connect(err=> {
+if (process.env.NODE_ENV === 'production') {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: process.env.DB_PORT || 3306,
+        host: process.env.DB_HOST || `localhost`,
+        password: process.env.DB_PASSWORD || `root`,
+        database: process.env.DB_DATABASE || `burger_db`,
+        user: process.env.DB_USER || `root`,
+    });
+}
+connection.connect(function (err) {
     if (err) {
-        console.error(`Error connecting: ${err.stack}`);
+        console.error("error connecting: " + err.stack);
         return;
     }
-    console.log(`Connected as id ${connection.threadId}`)
+    console.log("connected as id " + connection.threadId);
 });
-
 
 module.exports=connection;
 
